b448c90518126001c0fe75c7ffd10ac0aedb1388
Ben McCain <mccain.ben2@gmail.com>
2017-07-20 21:16:38 -0400

    updated store

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import { NEW_MESSAGE, GET_MESSAGES } from '../test/store.js'
 
-const combineActions = (mergeAction, finalAction) => store => next => action => {
+const combineActions = (toAdd, finalAction) => store => next => action => {
 
   //I need to get the state that specific action is looking to upate
   //add it to the state of the other one, which has a different name,
   //access the propert name I don't know...
   //access the name I don't know on the state...
 
-  if (action.type === mergeAction) {
+  if (action.type === toAdd) {
 
     console.log('action type inside...', action.type);
-    // let name = action.type.split('_').length
-    // console.log('test action type', name);
-    const state = store.getState();
+    let name = action.type.split('_').splice(-1)[0].toLowerCase();
+    console.log('test action type', name)
     // const mergedAction = {
     //   type: finalAction,
     //   payload: [ state.something, action.payload ]
--- test/store.js
+++ test/store.js
@@ -8,47 +8,50 @@ export const TEST = 'TEST';
 export const GET_MESSAGES = 'GET_MESSAGES';
 export const NEW_MESSAGE = 'NEW_MESSAGE';
 
-/* MiddleWare Created */
-
-let test = combineActions(NEW_MESSAGE, GET_MESSAGES);
 
 /* Initial State */
 export const initialState = {
-  test: '',
-  messages: ['how are you?'],
+    test: '',
+    messages: ['how are you?'],
 };
+
+/* MiddleWare Created */
+
+const message = combineActions(NEW_MESSAGE, GET_MESSAGES);
+const test = combineActions(TEST, GET_MESSAGES);
 
 /* Reducer Function */
 const testReducer = (state = initialState, action) => {
-  let newState = Object.assign({}, state);
-  switch (action.type) {
+    let newState = Object.assign({}, state);
+    switch (action.type) {
 
-    case TEST:
-        newState.test = action.payload
-        break;
+        case TEST:
+            newState.test = action.payload
+            break;
 
-    case RESET_STATE:
-        newState = initialState;
-        break;
+        case RESET_STATE:
+            newState = initialState;
+            break;
 
-    case GET_MESSAGES:
-        newState.messages = action.messages
-        break
+        case GET_MESSAGES:
+            newState.messages = action.messages
+            break
 
-    case NEW_MESSAGE:
-        newState.messages = [...state.messages, action.payload]
-        break
+        case NEW_MESSAGE:
+            newState.messages = [...state.messages, action.payload]
+            break
 
-    default:
-        return newState;
-  }
-  return newState;
+        default:
+            return newState;
+    }
+    return newState;
 }
 
 //store created
 const store = createStore(
-  testReducer,
-  applyMiddleware(test)
+    testReducer,
+    applyMiddleware(message,
+        test)
 );
 
 export default store;
