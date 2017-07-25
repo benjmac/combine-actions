87750e7dd3bd1baa176b739ae191fd26c33d6efe
Ben McCain <mccain.ben2@gmail.com>
2017-07-25 14:22:01 -0400

    updated function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 const combineActions = (toAdd, finalAction, stateProp) => store => next => action => {
 
-  //tests, think how I can break this...
-  //if the property isn't found?
   if (action.type === toAdd) {
 
     const mergedAction = {
       type: finalAction
     }
     const state = store.getState();
-    //first checks if state has the initial property we are looking to update
     let property = undefined;
     if (state[stateProp]) {
       property = state[stateProp];
